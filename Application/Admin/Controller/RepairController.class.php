b4972138e829275eb617b01ad89ce00091401fea
qin1994 <535192465@qq.com>
2017-08-10 22:13:10 +0800

    社区服务的在线维修和前台的登录

---
+++ Application/Admin/Controller/RepairController.class.php
@@ -0,0 +1,109 @@
+<?php
+
+
+namespace Admin\Controller;
+
+
+class RepairController extends AdminController
+{
+    public function index(){
+        $pid = i('get.pid', 0);
+        /* 获取频道列表 */
+        $map  = array('status' => array('gt', -1), 'pid'=>$pid);
+        $list = M('repairs')->where($map)->select();
+
+        $this->assign('list', $list);
+        $this->assign('pid', $pid);
+        $this->meta_title = '导航管理';
+        $this->display();
+    }
+
+    public function add(){
+        if(IS_POST){
+            $Repair = D('repairs');
+            $data = $Repair->create();
+            if($data){
+                $id = $Repair->add();
+                if($id){
+                    $this->success('新增成功', U('index'));
+                    //记录行为
+                     action_log('update_repair', 'repair', $id, UID);
+                } else {
+                    $this->error('新增失败');
+                }
+            } else {
+                $this->error($Repair->getError());
+            }
+        } else {
+            $pid = i('get.pid', 0);
+            //获取父导航
+            if(!empty($pid)){
+                $parent = M('repairs')->where(array('id'=>$pid))->field('title')->find();
+                $this->assign('parent', $parent);
+            }
+
+            $this->assign('pid', $pid);
+            $this->assign('info',null);
+            $this->meta_title = '新增导航';
+            $this->display('edit');
+        }
+    }
+
+    //删除
+    public function del(){
+        $id = array_unique((array)I('id',0));
+
+        if ( empty($id) ) {
+            $this->error('请选择要操作的数据!');
+        }
+
+        $map = array('id' => array('in', $id) );
+        if(M('repairs')->where($map)->delete()){
+            //记录行为
+            action_log('update_repair', 'repair', $id, UID);
+            $this->success('删除成功');
+        } else {
+            $this->error('删除失败！');
+        }
+    }
+
+    //编辑
+    public function edit($id = 0){
+        if(IS_POST){
+            $Repair = D('repairs');
+            $data = $Repair->create();
+            if($data){
+                if($Repair->save()){
+                    //记录行为
+                    action_log('update_repair', 'repair', $data['id'], UID);
+                    $this->success('编辑成功', U('index'));
+                } else {
+                    $this->error('编辑失败');
+                }
+
+            } else {
+                $this->error($Repair->getError());
+            }
+        } else {
+            $info = array();
+            /* 获取数据 */
+            $info = M('repairs')->find($id);
+
+            if(false === $info){
+                $this->error('获取配置信息错误');
+            }
+
+            $pid = i('get.pid', 0);
+            //获取父导航
+            if(!empty($pid)){
+                $parent = M('repairs')->where(array('id'=>$pid))->field('title')->find();
+                $this->assign('parent', $parent);
+            }
+
+            $this->assign('pid', $pid);
+            $this->assign('info', $info);
+            $this->meta_title = '编辑导航';
+            $this->display();
+        }
+    }
+}
